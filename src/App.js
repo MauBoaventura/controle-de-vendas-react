1c239520c4e5e1fccce91f2f108a0bb27eb36f99
Mauboa <mba097@gmail.com>
2021-05-14 18:23:59 -0300

    docs: adiciona endpoint produção

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const messages = {
 const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'pt-br');
 
 
-const dataProvider = jsonServerProvider(process.env.NODE_ENV != 'production'?'http://localhost:3035':'https://controledevenda.herokuapp.com/');
+const dataProvider = jsonServerProvider(process.env.NODE_ENV !== 'production'?'http://localhost:3035':'https://controledevenda.herokuapp.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider} loginPage={MyLoginPage} i18nProvider={i18nProvider}>
