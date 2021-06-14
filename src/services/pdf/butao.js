9f578a5e57742d303949121f3ef0e62f0b9513fd
Mauboa <mba097@gmail.com>
2021-06-14 18:34:22 -0300

    feat: adicao de novos relatorios

--- src/services/pdf/butao.js
+++ src/services/pdf/butao.js
@@ -12,13 +12,11 @@ const relatorioListaCobrancaDiaria = async () => {
 
     //Buscar dados da API 
     // Depois fazer uma consulta e jogar o processamento para a API
-    var pedido = (await client.get("/relatorioDiario"));
+    var pedido = (await client.get("/relatorioVencimentoDiario"));
     var data_hoje = moment().format("YYYY-MM-DD")
 
     var pedidos_vencendo_hoje = pedido.data.filter((element) => {
         let diaVencimento = moment(element.dataVencimentoPedido).format("YYYY-MM-DD")
-        // console.log(diaVencimento)
-        // console.log(diaVencimento === data_atual)
         return diaVencimento === data_hoje;
     }).map((el) => {
         el.quilo = el.quilo.toLocaleString('pt-BR', { style: 'decimal'})+ ' kg';
@@ -47,14 +45,90 @@ const relatorioListaCobrancaDiaria = async () => {
     }
 }
 
+const relatorioListaPedidosDoDia = async () => {
+
+    //Buscar dados da API 
+    // Depois fazer uma consulta e jogar o processamento para a API
+    var pedido = (await client.get("/relatorioPedidosDiario"));
+    console.log(pedido);
+  
+    var pedidos_feitos_hoje = pedido.data.map((el) => {
+        el.quilo = el.quilo.toLocaleString('pt-BR', { style: 'decimal'})+ ' kg';
+        el.valor = el.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.desconto = el.desconto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.totalDaNota = el.totalDaNota.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.dataVencimentoPedido = moment(el.dataVencimentoPedido).format("DD-MM-YYYY")
+        el.dataPedido = moment(el.dataPedido).format("DD-MM-YYYY")
+        return el
+    });
+
+    if (pedidos_feitos_hoje.length > 0) {
+        const classeImpressao = new Impressao(pedidos_feitos_hoje);
+        const documento = await classeImpressao.PreparaDocumento();
+        pdfMake.createPdf(documento)
+            // .download();
+            .open({}, window.open('', '_blank'));
+        // .print();
+    } else {
+        const classeImpressao = new Impressao(pedidos_feitos_hoje);
+        const documento = await classeImpressao.PreparaDocumento();
+        pdfMake.createPdf(documento)
+            // .download();
+            .open({}, window.open('', '_blank'));
+        console.error("Não ha pedidos feitos hoje");
+    }
+}
+
+const relatorioCadastroPedidosDoDia = async () => {
+
+    //Buscar dados da API 
+    // Depois fazer uma consulta e jogar o processamento para a API
+    var pedido = (await client.get("/relatorioPedidosCadastradosHoje"));
+    console.log(pedido);
+
+    var pedidos_feitos_hoje = pedido.data.map((el) => {
+        el.quilo = el.quilo.toLocaleString('pt-BR', { style: 'decimal'})+ ' kg';
+        el.valor = el.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.desconto = el.desconto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.totalDaNota = el.totalDaNota.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        el.dataVencimentoPedido = moment(el.dataVencimentoPedido).format("DD-MM-YYYY")
+        el.dataPedido = moment(el.dataPedido).format("DD-MM-YYYY")
+        return el
+    });
+
+    if (pedidos_feitos_hoje.length > 0) {
+        const classeImpressao = new Impressao(pedidos_feitos_hoje);
+        const documento = await classeImpressao.PreparaDocumento();
+        pdfMake.createPdf(documento)
+            // .download();
+            .open({}, window.open('', '_blank'));
+        // .print();
+    } else {
+        const classeImpressao = new Impressao(pedidos_feitos_hoje);
+        const documento = await classeImpressao.PreparaDocumento();
+        pdfMake.createPdf(documento)
+            // .download();
+            .open({}, window.open('', '_blank'));
+        console.error("Não ha pedidos feitos hoje");
+    }
+}
+
 function Butao(props) {
     return (<>
+    <ul>
         <button className="props" onClick={relatorioListaCobrancaDiaria}>
             Lista de Cobrança de hoje - {moment().format("DD/MM/YYYY")}
         </button>
-        <button className="props" onClick={relatorioListaCobrancaDiaria}>
-            Visualizar documento
+        <br/>
+        <button className="props" onClick={relatorioListaPedidosDoDia}>
+            Pedidos realizados hoje
         </button>
+        <br/>
+        <button className="props" onClick={relatorioCadastroPedidosDoDia}>
+            Pedidos cadastrados hoje
+        </button>
+        <br/>
+    </ul>
     </>
     )
 }
